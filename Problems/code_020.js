ed04344bafca7aa63e74d273b19969b518f7742f
marimotona <ssmmo.06021224@gmail.com>
2023-03-11 02:03:47 +0900

    readme

--- Problems/code_020.js
+++ Problems/code_020.js
@@ -1,8 +1,5 @@
 function factorial(num){
     if(num < 2) return 1n;
-    // let sum = 1;
-    // sum = num * factorial(num - 1);
-    // return sum;
     return BigInt(num) * factorial(num - 1);
 }
 
@@ -14,6 +11,5 @@ function sum(ele) {
     }
     return result;
 }
-//console.log(factorial(100));
 
 console.log(sum(factorial(100)));
