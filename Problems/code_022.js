abef63a78f073d176dce44b881a416c550092b67
marimotona <ssmmo.06021224@gmail.com>
2023-03-12 00:58:40 +0900

    code_022

--- Problems/code_022.js
+++ Problems/code_022.js
@@ -1,24 +1,34 @@
-// fetch('./code_022_names.txt')
-//     .then(res => res.text())
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     });
+//alphabetのデータを作る
+//let alphabet = [];
+let alphabet = {};
+for (let j = 'A'.charCodeAt(0); j <= 'Z'.charCodeAt(0); j++) {
+  //alphabet.push(String.fromCharCode([j]));
+  alphabet[String.fromCharCode(j)] = j - 'A'.charCodeAt(0) + 1;
+}
 
-// import {letter} from './code_022_names.text';
-// console.log(letter);
+//console.log(alphabet);
 
 const fs = require('fs');
 let letter = [];
+let sum = 0;
 fs.readFile('../Data/code_022_names.txt', 'utf-8', (err, data) => {
   if (err) throw err;
 //   letter.push(data);
 //   letter.sort();
   letter = data.replace(/"/g, '').split(',').sort();
-  console.log(letter);
+  const letterNum = letter;
+  console.log(letter.length);
+  for (let i = 0; i < letter.length; i ++) {
+    for (let k = i; k < letter[i].length; k ++) {
+      if(alphabet[letter[i][k]]) { //alphabetオブジェクトの中に、letter[i][k]の要素があっあたら
+        sum += alphabet[letter[i][k]];
+      }
+    }
+  }
+
+  console.log(sum);
 });
+
 
 //letter.sort();
 // let alphabet = letter.sort();
