b3dbb2c9429ee75f51b0f8862cc47ba88884a206
marimotona <ssmmo.06021224@gmail.com>
2023-04-05 23:32:17 +0900

    050

--- Problems/code_050.js
+++ Problems/code_050.js
@@ -10,3 +10,65 @@ The longest sum of consecutive primes below one-thousand that adds to a prime, c
 Which prime, below one-million, can be written as the sum of the most consecutive primes?
 
 */
+
+function eratosthenes(limit) {
+    const primes = [];
+    const isprime = new Array(limit + 1).fill(true);
+  
+    isprime[0] = false;
+    isprime[1] = false;
+  
+    for (let j = 2; j * j <= limit; j++) {
+      if (isprime[j]) {
+        for (let k = j * j; k <= limit; k += j) {
+          isprime[k] = false;
+        }
+      }
+    }
+  
+    for (let i = 2; i <= limit; i++) {
+      if (isprime[i]) {
+        primes.push(i);
+      }
+    }
+  
+    return primes;
+}
+
+function prime(N) {
+    if (N < 2) return false;
+    for(let i=2; i*i <= N; i++) {
+        if(N % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const primes = eratosthenes(1000000);
+
+function sumPrime(){
+    let big = 0;
+    let maxRun = 0;
+
+    for(let i = 0; i < primes.length; i++){
+        let sum = 0;
+        let run = 0;
+        for (let j = i; j < primes.length; j++){
+            sum += primes[j];
+            run++;
+
+            if (sum > 1000000) {
+                break;
+            }
+
+            if(prime(sum) && run > maxRun){
+                big = sum;
+                maxRun = run;
+            }
+        }
+    }
+    
+    return big;
+}
+console.log(sumPrime());
