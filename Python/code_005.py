6ff353dffb250d0dce0dcb09fb274ac2daa2d374
marimotona <ssmmo.06021224@gmail.com>
2023-04-17 23:08:47 +0900

    005

--- Python/code_005.py
+++ Python/code_005.py
@@ -2,3 +2,18 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+import math
+
+def lcm(a, b):
+    return abs(a * b) // math.gcd(a, b)
+
+def find_lcm_of_range(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = lcm(result, i)
+    return result
+
+smallest_number = find_lcm_of_range(20)
+print(smallest_number)
+
+    
