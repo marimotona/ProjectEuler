c130817a79cb7153d973b8b96f36b356378e3d2d
marimotona <ssmmo.06021224@gmail.com>
2023-06-08 00:30:15 +0900

    code_008

---
+++ Python/code_008.py
@@ -0,0 +1,13 @@
+with open('../Data/euler_008.txt', 'r') as f:
+    data = f.read().replace('\n', '')
+
+max_prduct = 0
+
+for i in range(len(data) - 12):
+    digits = [int(d) for d in data[i:i+13]]
+    product = 1
+    for d in digits:
+        product *= d
+    if product > max_prduct:
+        max_prduct = product
+print(max_prduct)
