e5bd850c95009446decb85e3483f4a0e7841bc25
marimotona <ssmmo.06021224@gmail.com>
2023-06-08 00:31:39 +0900

    simpleに変更

--- Python/code_008.py
+++ Python/code_008.py
@@ -3,11 +3,18 @@ with open('../Data/euler_008.txt', 'r') as f:
 
 max_prduct = 0
 
+# for i in range(len(data) - 12):
+#     digits = [int(d) for d in data[i:i+13]]
+#     product = 1
+#     for d in digits:
+#         product *= d
+#     if product > max_prduct:
+#         max_prduct = product
+
 for i in range(len(data) - 12):
-    digits = [int(d) for d in data[i:i+13]]
     product = 1
-    for d in digits:
-        product *= d
-    if product > max_prduct:
-        max_prduct = product
+    for d in data[i:i+13]:
+        product *= int(d)
+    max_prduct = max(max_prduct, product)
+
 print(max_prduct)
